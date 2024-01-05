926d6efec8a42c58b0274b337dd0431cb4ba77ed
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-05 20:38:57 +0800

    chore(rehype-custom-emoji): update basic test

--- packages/rehype-custom-emoji/test/basic.spec.ts
+++ packages/rehype-custom-emoji/test/basic.spec.ts
@@ -4,7 +4,7 @@ import remarkRehype from 'remark-rehype'
 import { unified } from 'unified'
 import { describe, expect, it } from 'vitest'
 
-import { rehypeCustomEmoji } from '../src'
+import { h, rehypeCustomEmoji } from '../src'
 
 describe('basic', () => {
   it('basic', async () => {
@@ -21,5 +21,25 @@ describe('basic', () => {
       .process('Hello, :custom_emoji:!')
 
     expect(basic.value).toBe('<p>Hello, <picture><source media="(prefers-reduced-motion: no-preference)" srcset="https://example.com/custom_emoji.png"><img alt=":custom_emoji:" src="https://example.com/custom_emoji.png" style="aspect-ratio:1/1;height:1em;vertical-align:text-top" title=":custom_emoji:"></picture>!</p>')
+  })
+
+  it('custom format', async () => {
+    const basic = await unified()
+      .use(remarkParse)
+      .use(remarkRehype)
+      .use(rehypeCustomEmoji, {
+        emojis: [{
+          shortcode: 'custom_emoji',
+          url: 'https://example.com/custom_emoji.png',
+        }],
+        format: (emoji) => h('img', {
+          alt: `:${emoji.shortcode}:`,
+          src: emoji.url,
+        }),
+      })
+      .use(rehypeStringify)
+      .process('Hello, :custom_emoji:!')
+
+    expect(basic.value).toBe('<p>Hello, <img alt=":custom_emoji:" src="https://example.com/custom_emoji.png">!</p>')
   })
 })
