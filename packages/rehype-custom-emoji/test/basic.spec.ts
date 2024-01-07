ce7ec76b84e1906c5f4a029ca13321cecd1f8ea7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-07 23:05:55 +0800

    chore: update unit test

--- packages/rehype-custom-emoji/test/basic.spec.ts
+++ packages/rehype-custom-emoji/test/basic.spec.ts
@@ -7,39 +7,30 @@ import { describe, expect, it } from 'vitest'
 import { h, rehypeCustomEmoji } from '../src'
 
 describe('basic', () => {
-  it('basic', async () => {
-    const basic = await unified()
-      .use(remarkParse)
-      .use(remarkRehype)
-      .use(rehypeCustomEmoji, {
-        emojis: [{
-          shortcode: 'custom_emoji',
-          url: 'https://example.com/custom_emoji.png',
-        }],
-      })
-      .use(rehypeStringify)
-      .process('Hello, :custom_emoji:!')
+  const processor = unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeCustomEmoji, {
+      emojis: [{
+        shortcode: 'custom_emoji',
+        url: 'https://example.com/custom_emoji.png',
+      }],
+      format: (emoji) => h('img', {
+        alt: `:${emoji.shortcode}:`,
+        src: emoji.url,
+      }),
+    })
+    .use(rehypeStringify)
 
-    expect(basic.value).toBe('<p>Hello, <picture><source media="(prefers-reduced-motion: no-preference)" srcset="https://example.com/custom_emoji.png"><img alt=":custom_emoji:" src="https://example.com/custom_emoji.png" style="aspect-ratio:1/1;height:1em;vertical-align:text-top" title=":custom_emoji:"></picture>!</p>')
+  it('basic', async () => {
+    const file = await processor.process('Hello, :custom_emoji:!')
+
+    expect(file.value).toBe('<p>Hello, <picture><source media="(prefers-reduced-motion: no-preference)" srcset="https://example.com/custom_emoji.png"><img alt=":custom_emoji:" src="https://example.com/custom_emoji.png" style="aspect-ratio:1/1;height:1em;vertical-align:text-top" title=":custom_emoji:"></picture>!</p>')
   })
 
   it('custom format', async () => {
-    const basic = await unified()
-      .use(remarkParse)
-      .use(remarkRehype)
-      .use(rehypeCustomEmoji, {
-        emojis: [{
-          shortcode: 'custom_emoji',
-          url: 'https://example.com/custom_emoji.png',
-        }],
-        format: (emoji) => h('img', {
-          alt: `:${emoji.shortcode}:`,
-          src: emoji.url,
-        }),
-      })
-      .use(rehypeStringify)
-      .process('Hello, :custom_emoji:!')
+    const file = await processor.process('Hello, :custom_emoji:!')
 
-    expect(basic.value).toBe('<p>Hello, <img alt=":custom_emoji:" src="https://example.com/custom_emoji.png">!</p>')
+    expect(file.value).toBe('<p>Hello, <img alt=":custom_emoji:" src="https://example.com/custom_emoji.png">!</p>')
   })
 })
--- packages/rehype-custom-emoji/test/mastodon.spec.ts
+++ packages/rehype-custom-emoji/test/mastodon.spec.ts
@@ -7,29 +7,28 @@ import { describe, expect, it } from 'vitest'
 import { category, one, rehypeCustomEmoji } from '../src'
 
 describe('mastodon', () => {
+  const processor = unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+
   it('mozilla.social', async () => {
-    const mastodon = await unified()
-      .use(remarkParse)
-      .use(remarkRehype)
+    const file = await processor
       .use(rehypeCustomEmoji, {
         emojis: await category(new URL('https://mozilla.social'), 'Mozilla'),
       })
-      .use(rehypeStringify)
       .process('Hello, :firefox:!')
 
-    expect(mastodon.value).toBe('<p>Hello, <picture><source media="(prefers-reduced-motion: no-preference)" srcset="https://assets.mozilla.social/custom_emojis/images/000/017/290/original/f1431a56d0ce4ad2.png"><img alt=":firefox:" src="https://assets.mozilla.social/custom_emojis/images/000/017/290/static/f1431a56d0ce4ad2.png" style="aspect-ratio:1/1;height:1em;vertical-align:text-top" title=":firefox:"></picture>!</p>')
+    expect(file.value).toBe('<p>Hello, <picture><source media="(prefers-reduced-motion: no-preference)" srcset="https://assets.mozilla.social/custom_emojis/images/000/017/290/original/f1431a56d0ce4ad2.png"><img alt=":firefox:" src="https://assets.mozilla.social/custom_emojis/images/000/017/290/static/f1431a56d0ce4ad2.png" style="aspect-ratio:1/1;height:1em;vertical-align:text-top" title=":firefox:"></picture>!</p>')
   })
 
   it('m.webtoo.ls', async () => {
-    const mastodon = await unified()
-      .use(remarkParse)
-      .use(remarkRehype)
+    const file = await processor
       .use(rehypeCustomEmoji, {
         emojis: [await one('https://m.webtoo.ls', 'vitest')],
       })
-      .use(rehypeStringify)
       .process('Hello, :vitest:!')
 
-    expect(mastodon.value).toBe('<p>Hello, <picture><source media="(prefers-reduced-motion: no-preference)" srcset="https://media.webtoo.ls/custom_emojis/images/000/000/380/original/bd890892e07d0f17.png"><img alt=":vitest:" src="https://media.webtoo.ls/custom_emojis/images/000/000/380/static/bd890892e07d0f17.png" style="aspect-ratio:1/1;height:1em;vertical-align:text-top" title=":vitest:"></picture>!</p>')
+    expect(file.value).toBe('<p>Hello, <picture><source media="(prefers-reduced-motion: no-preference)" srcset="https://media.webtoo.ls/custom_emojis/images/000/000/380/original/bd890892e07d0f17.png"><img alt=":vitest:" src="https://media.webtoo.ls/custom_emojis/images/000/000/380/static/bd890892e07d0f17.png" style="aspect-ratio:1/1;height:1em;vertical-align:text-top" title=":vitest:"></picture>!</p>')
   })
 })
