60df49e384418b17c1e4b67d4f4121ccdff59a15
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-07 23:20:16 +0800

    refactor(remark-webfinger-mentions): custom trim

--- packages/remark-webfinger-mentions/src/plugin.ts
+++ packages/remark-webfinger-mentions/src/plugin.ts
@@ -8,16 +8,18 @@ import { getAcct } from './fetcher'
 
 export type RemarkWebfingerMentionsOptions = {
   match?: RegExp
+  trim?: (str: string) => string
 }
 
 export const remarkWebfingerMentions: Plugin<[RemarkWebfingerMentionsOptions], Root, Root> = (options): Transformer<Root, Root> => {
   const resources = new Map<string, string | undefined>()
   const match = options.match ?? /@([\w%+.-]+)@([\w.-]+\.[\da-z-]{2,})/g
+  const trim = options.trim ?? ((str: string) => str.slice(1)) // @user@example.com => user@example.com
 
   return (tree) => findAndReplace(tree, [
     match,
     (str: string) => {
-      const acct = str.slice(1)
+      const acct = trim(str)
       let url: string | undefined
 
       if (resources.has(acct)) {
